d6d11501e2501e72ce2cc6fb4c6126cb5f6613df
Sabin Lc <sabinlc47@gmail.com>
2024-04-22 15:44:44 +0545

    API Implement

---
+++ quotegenerator/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sedan: ["Sedan"],
+        josefin: ["Josefin Sans"],
+        playfair: ["Playfair Display"],
+      },
+      animation: {
+        shine: "shine 1s",
+      },
+      keyframes: {
+        shine: {
+          "100%": { left: "125%" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
