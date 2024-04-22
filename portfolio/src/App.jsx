4e61ada35d68d856e3ebb74c08ff33353a3e3539
Sabin Lc <sabinlc47@gmail.com>
2024-04-22 11:31:01 +0545

    Added Footer

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,17 +4,18 @@ import Body from "./Components/Body";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-
         <Body />
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </Router>
     </>
   );
