c4c87b5b1f414c264d622554520aad6c2ae006c0
Sabin Lc <sabinlc47@gmail.com>
2024-04-24 16:03:02 +0545

    Cloned with vite react and css

---
+++ gemini-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Main from "./Components/Main/Main";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
